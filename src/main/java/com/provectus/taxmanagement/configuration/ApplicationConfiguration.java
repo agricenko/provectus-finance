6dc4975eefe6cd886fc7e038ec4db1cd2fa10ec3
Alexey Shevchuk <ashevchuk@determine.com>
2017-03-11 21:02:13 +0200

    Added mongodb, unit tests for entity logic, integration tests

---
+++ src/main/java/com/provectus/taxmanagement/configuration/ApplicationConfiguration.java
@@ -0,0 +1,29 @@
+package com.provectus.taxmanagement.configuration;
+
+import com.mongodb.MongoClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+/**
+ * Created by alexey on 11.03.17.
+ */
+@Configuration
+@EnableMongoRepositories(basePackages = "com.provectus.taxmanagement.repository")
+public class ApplicationConfiguration {
+
+    @Bean
+    public MongoOperations mongoTemplate() {
+        MongoClient mongoClient = new MongoClient();
+        return new MongoTemplate(new SimpleMongoDbFactory(mongoClient, "provectus"));
+    }
+
+    @Bean
+    public MongoMappingContext mappingContext() {
+        return new MongoMappingContext();
+    }
+}
